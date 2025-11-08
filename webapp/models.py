b469e5e9c5f19d9b68707f4220fd23d23f248ae0
Talgat <JorobekovT@gmail.com>
2025-11-08 12:40:37 +0600

    создал модель GuestBookReview

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class GuestBookReview(models.Model):
+    status_choices = [
+        ('active', 'active'),
+        ('blocked', 'blocked'),
+    ]
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=status_choices, default='active')
+
+    def _str_(self):
+        return f'{self.name} ({self.email})'
