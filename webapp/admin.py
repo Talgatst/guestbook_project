361116a40615dec629b6d97fa6645eec720f233f
Talgat <JorobekovT@gmail.com>
2025-11-08 12:41:10 +0600

    зарегистрировал модель GuestBookReview в админ панеле

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from webapp.models import GuestBookReview
 
-# Register your models here.
+
+@admin.register(GuestBookReview)
+class GuestBookReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('name', 'email', 'text')
